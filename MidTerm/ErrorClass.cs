a47e2e8adb1ab61b71ea7cd84fbda14c76c4a51b
Tales <tales.trab@gmail.com>
2022-03-16 20:44:14 -0500

    Initial Commit

---
+++ MidTerm/ErrorClass.cs
@@ -0,0 +1,15 @@
+﻿namespace MidTerm
+{
+    public class ErrorClass
+    {
+        public string? Details { get; set; }
+        public string? Message { get; set; }
+        public List<ValidationErrors>? ValidationErrors { get; set; }
+    }
+    public class ValidationErrors
+    {
+        public bool IsValid { get; set; }
+        public string? RestrictionType { get; set; }
+        public string? RuleName { get; set; }
+    }
+}
---
+++ MidTerm/Program.cs
@@ -0,0 +1,21 @@
+﻿using MidTerm;
+using System.Text.Json;
+
+var path = Path.Combine(@"C:\Users\user\source\", "midterm.txt");
+string content = File.ReadAllText(path);
+
+ErrorClass? errorClass = JsonSerializer.Deserialize<ErrorClass>(content);
+
+Console.WriteLine($"Details: {errorClass?.Details}");
+Console.WriteLine($"Message: {errorClass?.Message}");
+var count = 1;
+foreach (var erro in errorClass.ValidationErrors)
+{   
+    Console.WriteLine($"Erro #:{count}");
+    Console.WriteLine($"Is Valid: {erro.IsValid}");
+    Console.WriteLine($"Restriction Type: {erro.RestrictionType}");
+    Console.WriteLine($"Rule Name: {erro.RuleName}");
+    count ++;
+}
+
+
